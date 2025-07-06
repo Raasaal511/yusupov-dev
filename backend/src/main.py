ec2a4863c6a0c24d04c069cb95739dd3d17a9169
Расул Юсупов <rasul.iusupov@quniq.net>
2025-07-06 12:19:25 +0300

    Update files

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from features.auth.routers import auth_app
 from features.admin.routers import admin_app
 from features.users.routers import user_app
+from features.content.routers import content_app
 
 
 app = FastAPI()
@@ -11,6 +12,7 @@ app = FastAPI()
 app.include_router(auth_app)
 app.include_router(admin_app)
 app.include_router(user_app)
+app.include_router(content_app)
 
 if __name__ == "__main__":
     uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True) # используется в dev версии
